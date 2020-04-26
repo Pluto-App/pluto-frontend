5159416a4a8d4cef1d6503edee3e3b5007b323e8
codersguild <shekharhiran@hotmail.com>
2020-04-26 11:06:44 +0530

    Basic Login Flow

---
+++ src/components/topBar.js
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import { useOvermind } from '../overmind'
+const { remote } = window.require('electron');
+const { ipcRenderer } = window.require('electron')
+const { app } = window.require('electron')
+
+// TODO Need to show some tooltip using Tailwind CSS ToolTip
+const TopBar = (props) => {
+  
+  const { state, actions, effects, reaction } = useOvermind()
+
+  const minimize = () => {
+    var window = remote.getCurrentWindow();
+    window.minimize();
+  }
+
+  const close = () => {
+    // TODO need to close all windows here. 
+    var window = remote.getCurrentWindow();
+    window.close();
+    // ipcRenderer.send('window-all-closed');
+    // app.quit()
+  }
+
+  return (
+      <div className="topBar" style={{height: "30px", width:"100%", background:"#000" }}>
+        <div className="flex justify-between items-center px-2 p-0">
+        <button className="text-white cursor-pointer hover:bg-gray-800">
+          <i className="material-icons md-light md-inactive" style={{ fontSize: "18px" }}> menu </i>
+        </button>
+        <div class="flex-1 draggable-elem" style={{ height: "30px" }}>
+        </div>
+        <div className="flex items-center">
+          <button className="text-white hover:bg-gray-800" style= {{ marginRight: "15px" }}> 
+            <i className="material-icons md-light md-inactive" onMouseOver={() => {}} onClick={() => {minimize()}} style={{ fontSize: "18px", margin: "0", marginTop: "-15px" }}> minimize </i></button>
+          <button className="text-white hover:bg-gray-800" >
+            <i className="material-icons md-light md-inactive" onClick={() => {close()}} style={{ fontSize: "18px", margin: "0" }}> close </i>
+          </button>
+        </div>
+        </div>
+      </div>
+  );
+}
+
+export default TopBar;
--- src/overmind/state.js
+++ src/overmind/state.js
@@ -1,4 +1,7 @@
 export const state = {
     title: 'Pluto App',
-    name: 'Pluto Dev Team'
+    name: 'Pluto Dev Team',
+    signUptxt : "Sign Up Success",
+    signedIn : false,
+    loggedIn : false, 
   }
