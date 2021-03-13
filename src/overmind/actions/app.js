7bc1ee583434cf7433ec4924ecd8aed9cd73f66a
Sambhav Sharma <sambhav.sharma@booking.com>
2021-03-13 14:48:57 +0100

    sharing screen main state

--- src/overmind/actions/app.js
+++ src/overmind/actions/app.js
@@ -89,7 +89,7 @@ export const userVideoCall = async ({ state, effect }, data) => {
 
 export const userScreenShare = async ({ state, effect }, data) => {
 
-	if(data.sender_id === state.userProfileData.uid){
+	if(data.sender_id !== state.userProfileData.uid){
 	 	localStorage.setItem("screenshare_channel_id", data.channel_id);
 	 	localStorage.setItem("screenshare_resolution", JSON.stringify(data.resolution));
 	 	localStorage.setItem("screenshare_owner", data.sender_id);
